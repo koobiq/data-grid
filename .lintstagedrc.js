c4388df5d83598a84dc808303a7a19eed6e1c7a2
Artem Belik <artyombelyk@gmail.com>
2025-03-27 16:37:29 +0300

    refactor: added stylelint config

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,3 +1,5 @@
 module.exports = {
-    '*.{css,scss,md,yml,json,js,ts,html}': 'prettier --write'
+    '*.{css,scss,md,yml,json,js,ts,html}': 'prettier --write',
+    '*.{js,ts,html}': 'eslint --max-warnings=0 --fix',
+    '*.{css,scss}': 'stylelint --max-warnings=0 --fix'
 };
---
+++ .stylelintrc.js
@@ -0,0 +1,15 @@
+// @ts-check
+
+/** @type {import('stylelint').Config} */
+const config = {
+    defaultSeverity: 'error',
+    allowEmptyInput: true,
+    extends: [
+        'stylelint-config-recommended-scss',
+        // should be last
+        'stylelint-prettier/recommended'
+    ],
+    rules: {}
+};
+
+module.exports = config;
