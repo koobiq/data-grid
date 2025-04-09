92b6c5db3e268ad5482b00aca121488c632e72e6
Artem Belik <artyombelyk@gmail.com>
2025-04-09 17:58:37 +0300

    chore: added dependabot config

--- .commitlintrc.js
+++ .commitlintrc.js
@@ -1,8 +1,31 @@
 // @ts-check
 
+const { readdirSync } = require('fs');
+const { resolve } = require('path');
+
+const makeScopeTypesByPath = (path) => {
+    const files = readdirSync(resolve(__dirname, path), { withFileTypes: true });
+    const directories = files.filter((file) => file.isDirectory());
+    return directories.map((dir) => dir.name);
+};
+
 /** @type {import('@commitlint/types').UserConfig} */
 const config = {
-    extends: ['@commitlint/config-conventional']
+    extends: ['@commitlint/config-conventional'],
+    rules: {
+        'header-max-length': [2, 'always', 120],
+        'scope-enum': [
+            2,
+            'always',
+            [
+                ...makeScopeTypesByPath(resolve(__dirname, 'packages')),
+
+                // Dependabot scopes
+                'deps',
+                'deps-dev'
+            ]
+        ]
+    }
 };
 
 module.exports = config;
