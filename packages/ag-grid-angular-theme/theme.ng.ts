a2be8f730644017c9f577453f4901699dc3b03c7
Artem Belik <artyombelyk@gmail.com>
2025-06-24 17:46:39 +0300

    feat(ag-grid-angular-theme): added disableCellFocusStyles input (#DS-3614) (#64)

--- packages/ag-grid-angular-theme/theme.ng.ts
+++ packages/ag-grid-angular-theme/theme.ng.ts
@@ -1,10 +1,18 @@
-import { Directive } from '@angular/core';
+import { booleanAttribute, Directive, input } from '@angular/core';
 
 @Directive({
     standalone: true,
     selector: 'ag-grid-angular[kbqAgGridTheme]',
     host: {
-        class: 'ag-theme-koobiq'
+        class: 'ag-theme-koobiq',
+        '[class.ag-theme-koobiq_disable-cell-focus-styles]': 'disableCellFocusStyles()'
     }
 })
-export class KbqAgGridTheme {}
+export class KbqAgGridTheme {
+    /**
+     * Disables ag-grid cell focus styles (e.g. border-color).
+     *
+     * @default false
+     */
+    readonly disableCellFocusStyles = input(false, { transform: booleanAttribute });
+}
