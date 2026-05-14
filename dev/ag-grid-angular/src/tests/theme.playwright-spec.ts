5962c3caf3451b977f889da701fa73b55ebaf487
Artem Belik <artyombelyk@gmail.com>
2026-05-14 16:59:13 +0300

    fix(ag-grid-angular-theme): dropdown background (#DS-5077) (#163)
    Co-authored-by: artembelik <24925224+artembelik@users.noreply.github.com>

--- dev/ag-grid-angular/src/tests/theme.playwright-spec.ts
+++ dev/ag-grid-angular/src/tests/theme.playwright-spec.ts
@@ -27,4 +27,15 @@ test.describe('KbqAgGridAngularTheme', () => {
             .evaluate((element: HTMLElement) => element.scrollTo({ left: 10 }));
         await expect(getScreenshotTarget(page)).toHaveScreenshot('theme-pinned-columns-light.png');
     });
+
+    test('with opened filter popup', async ({ page }) => {
+        await page.setViewportSize({ width: 500, height: 500 });
+        await page.goto('/e2e/theme');
+        await page.hover('.ag-header-cell[col-id="athlete"]');
+        await page.click('.ag-header-cell[col-id="athlete"] .ag-header-cell-filter-button');
+        await page.click('.ag-menu .ag-filter-select');
+        await expect(getScreenshotTarget(page)).toHaveScreenshot('theme-filter-popup-light.png');
+        await enableDarkTheme(page);
+        await expect(getScreenshotTarget(page)).toHaveScreenshot('theme-filter-popup-dark.png');
+    });
 });
